ac110d12aae653a93c20e720cb07fbfd594bab5d
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-09 15:43:19 +0300

    Initial Commit

---
+++ NewProjectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewProjectApp
+//
+//  Created by Александр Кузьмичев on 09.10.2020.
+//  Copyright © 2020 Александр Кузьмичев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
