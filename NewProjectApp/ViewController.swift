6c8df0de790ac0a46d665c1823f4b7c5ec59416c
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-13 13:33:46 +0300

    Add outlets Start button action

--- NewProjectApp/ViewController.swift
+++ NewProjectApp/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLable: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLable.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPrassed() {
+        if helloWorldLable.isHidden {
+        helloWorldLable.isHidden = false
+        startButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloWorldLable.isHidden = true
+            startButton.setTitle("START", for: .normal)
+        }
+    }
+    
 }
 
